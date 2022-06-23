8c14880bbc4c7f94b6daef0658983fd4b680cc98
Pier95F <forcinapierpaolo@gmail.com>
2022-06-23 15:04:25 +0200

    terminata classe smartphone

--- src/jana60/shop/Smartphone.java
+++ src/jana60/shop/Smartphone.java
@@ -1,5 +1,46 @@
 package jana60.shop;
 
-public class Smartphone {
+public class Smartphone extends Prodotto {
+
+	
+	// Definisco gli attributi
+	
+	private String imei;
+	private String memoria;
+	
+	// Costruttori 
+	
+	public Smartphone(int codice, String nome, String marca, double prezzo, double iva, String imei, String memoria) {
+		super(codice, nome, marca, prezzo, iva);
+		this.imei = imei;
+		this.memoria = memoria;		
+	}
+	
+	// Imposto i getters e i setters
+
+	public String getImei() {
+		return imei;
+	}
+
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+
+	public String getMemoria() {
+		return memoria;
+	}
+
+	public void setMemoria(String memoria) {
+		this.memoria = memoria;
+	}
+	
+	// Override
+	public String toString () {
+		return super.toString() + ", codice IMEI: " + imei + ", memoria interna: " + memoria;
+	}	
+
+	
+	
+	
 
 }
